5cb1352a9f99822c0307606da1ca8068aac643b2
kanebassirou <kaneb1927@gmail.com>
2023-07-18 19:21:18 +0000

    mise de la page web.php en utilisant route groupe

--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::group(['middleware' => 'web'], function () {
-    // Routes du contrôleur HomeController
-    Route::get('/', [HomeController::class, 'home'])->name('app_home');
-    Route::get('/about', [HomeController::class, 'about'])->name('app_about');
-    Route::match(['get', 'post'], '/dashboard', [HomeController::class, 'dashboard'])
-        ->middleware('auth')
-        ->name('app_dashboard');
-
-    // Routes du contrôleur LoginController
-    Route::get('/logout', [LoginController::class, 'logout'])->name('app_logout');
-    Route::post('/exist_email', [LoginController::class, 'existEmail'])->name('app_existEmail');
-    Route::match(['get', 'post'], '/activation_code/{token}', [LoginController::class, 'activationCode'])->name('app_activation_code');
-    Route::get('/user_checher', [LoginController::class, 'userChecher'])->name('app_user_checher');
-    Route::get('/resend_activation_code/{token}', [LoginController::class, 'resendActivationCode'])->name('app_resend_activation_code');
-    Route::get('/activation_account_link/{token}', [LoginController::class, 'activationAccountLink'])->name('app_activation_account_link');
-    Route::match(['get', 'post'], '/activation_account_change_email/{token}', [LoginController::class, 'activationAccountChangeEmail'])->name('app_activation_account_change_email');
+// les routes pour controller HomeController
+Route::controller(HomeController::class)->group(function(){
+    Route::get('/', 'home')->name('app_home');
+    Route::get('/about','about')->name('app_about');
+    Route::match(['get','post'],'/dashboard','dashboard')->middleware('auth')
+    ->name('app_dashboard');//utilisateur ne peut pas acceder a la page dashbord si il n'est pas connecter
 });
 
+// les routes pour controller LoginController
+Route::controller(LoginController::class)->group(function(){
+    Route::get('/logout','logout')->name('app_logout');
+    Route::post('/exist_email','existEmail')->name('app_existEmail');
+    Route::match(['get', 'post'], '/activation_code/{token}','activationCode')->name('app_activation_code');
+    Route::get('/user_checher','userChecher')->name('app_user_checher');
+    Route::get('/resend_activation_code/{token}',('resendActivationCode'))->name('app_resend_activation_code');
+    Route::get('/activation_account_link/{token}',('activationAccountLink'))->name('app_activation_account_link');
+    Route::match(['get', 'post'], '/activation_account_change_email/{token}',('activationAccountChangeEmail'))->name('app_activation_account_change_email');
+
+});
 
 // Route::get('/', [HomeController::class,'home'])
 // ->name('app_home');
@@ -43,6 +43,8 @@ Route::group(['middleware' => 'web'], function () {
 // //utilisateur ne peut pas acceder a la page dashbord si il n'est pas connecter
 // ->middleware('auth')
 // ->name('app_dashboard');
+
+
 // Route::get('/logout',[LoginController::class,'logout'])
 // ->name('app_logout');
 // Route::post('/exist_email',[LoginController::class,'existEmail'])
